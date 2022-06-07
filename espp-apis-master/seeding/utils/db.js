0aec7ea06d5f7816f2f1f9686605261c97c778cd
Jagmohan-Bodra <45682525+Jagmohan-Bodra@users.noreply.github.com>
2022-06-07 07:31:52 +0530

    Update db.js

--- espp-apis-master/seeding/utils/db.js
+++ espp-apis-master/seeding/utils/db.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-const MONGODB__CONNECTION_STRING="mongodb://user:passwww3@127.0.0.1:27017/dbtest?authSource=admin&readPreference=primary&ssl=false"
+const MONGODB__CONNECTION_STRING="mongodb://user:passwww3@0.0.0.0:27017/dbtest?authSource=admin&readPreference=primary&ssl=false"
 const MONGODB__NAME="dbtest"
 
 let dbConnect;
